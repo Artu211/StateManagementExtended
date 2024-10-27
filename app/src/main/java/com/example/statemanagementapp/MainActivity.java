15b480754f7418c0611e94a51c3966fbf34b1a61
Artu211 <160549068+Artu211@users.noreply.github.com>
2024-10-27 18:04:21 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/statemanagementapp/MainActivity.java
+++ app/src/main/java/com/example/statemanagementapp/MainActivity.java
@@ -9,58 +9,91 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.graphics.Color;
 import android.widget.CompoundButton;
-import androidx.lifecycle.ViewModelProvider;
 import android.content.res.ColorStateList;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Switch;
-
 
 public class MainActivity extends AppCompatActivity {
     private static final String KEY_COUNT = "count";
     private static final String KEY_INPUT = "input1";
     private static final String KEY_CHECK = "check1";
     private static final String KEY_SWITCH = "switch1";
+    private static final String KEY_DARK_MODE = "darkMode"; // Dodana stała do zapisu trybu
+
     private TextView textViewCount;
-    private int count =0;
-    private String input2;
-    private Boolean check2;
-    private Boolean switch2;
+    private int count = 0;
     private Switch switcher;
     private EditText input;
     private TextView textViewOpcjaZaznaczona;
     private CheckBox checkbox;
     private LinearLayout main;
+    private Button buttonIncrement;
+    private boolean isDarkMode = false; // Flaga trybu ciemnego
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
-        textViewCount=findViewById(R.id.text);
-        Button buttonIncrement = findViewById(R.id.button);
-        if(savedInstanceState !=null){
-            count = savedInstanceState.getInt(KEY_COUNT);
-            input2 = savedInstanceState.getString(KEY_INPUT);
-            check2 = savedInstanceState.getBoolean(KEY_CHECK);
-            switch2 = savedInstanceState.getBoolean(KEY_SWITCH);
-        }
-        updateCountText();
 
-        buttonIncrement.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v){
-                count++;
-                updateCountText();
-            }
-
-        });
-
-
+        textViewCount = findViewById(R.id.text);
+        buttonIncrement = findViewById(R.id.button);
         switcher = findViewById(R.id.switcher);
         main = findViewById(R.id.main_layout);
         input = findViewById(R.id.input);
-            buttonIncrement.setBackgroundColor(Color.BLUE);
-        // Ustawienie słuchacza zmiany stanu i gdy przełączymy to wykona się ten kod i zrobi nam wszystko pod tryb ciemny
-        switcher.setOnCheckedChangeListener((buttonView, isChecked) -> {if (isChecked) {
+        checkbox = findViewById(R.id.check);
+        textViewOpcjaZaznaczona = findViewById(R.id.inform);
+
+        // Przywracanie stanów po obrocie
+        if (savedInstanceState != null) {
+            count = savedInstanceState.getInt(KEY_COUNT);
+            input.setText(savedInstanceState.getString(KEY_INPUT));
+            checkbox.setChecked(savedInstanceState.getBoolean(KEY_CHECK));
+            switcher.setChecked(savedInstanceState.getBoolean(KEY_SWITCH));
+            isDarkMode = savedInstanceState.getBoolean(KEY_DARK_MODE, false);
+        }
+        updateCountText();
+        applyTheme();  // Ustawienia kolorów na podstawie isDarkMode
+
+        buttonIncrement.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                count++;
+                updateCountText();
+            }
+        });
+
+        // Słuchacz dla przełącznika trybu ciemnego
+        switcher.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            isDarkMode = isChecked;  // Aktualizacja stanu trybu
+            applyTheme();
+        });
+
+        // Słuchacz dla checkboxa
+        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                textViewOpcjaZaznaczona.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+            }
+        });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_COUNT, count);
+        outState.putString(KEY_INPUT, input.getText().toString());
+        outState.putBoolean(KEY_CHECK, checkbox.isChecked());
+        outState.putBoolean(KEY_SWITCH, switcher.isChecked());
+        outState.putBoolean(KEY_DARK_MODE, isDarkMode);  // Zapisanie trybu ciemnego
+    }
+
+    private void updateCountText() {
+        textViewCount.setText("Licznik: " + count);
+    }
+
+    // Funkcja zmieniająca motyw
+    private void applyTheme() {
+        if (isDarkMode) {
             main.setBackgroundColor(Color.BLACK);
             textViewCount.setTextColor(Color.WHITE);
             input.setTextColor(Color.WHITE);
@@ -69,10 +102,8 @@ public class MainActivity extends AppCompatActivity {
             checkbox.setTextColor(Color.WHITE);
             textViewOpcjaZaznaczona.setTextColor(Color.WHITE);
             switcher.setTextColor(Color.WHITE);
-            switcher.setThumbTintList(ColorStateList.valueOf(Color.WHITE)); // Kolor główki Switcha
+            switcher.setThumbTintList(ColorStateList.valueOf(Color.WHITE));
             switcher.setTrackTintList(ColorStateList.valueOf(Color.GRAY));
-            buttonIncrement.setTextColor(Color.BLACK);
-            buttonIncrement.setBackgroundColor(Color.WHITE);
         } else {
             main.setBackgroundColor(Color.WHITE);
             textViewCount.setTextColor(Color.BLACK);
@@ -80,39 +111,7 @@ public class MainActivity extends AppCompatActivity {
             checkbox.setTextColor(Color.BLACK);
             textViewOpcjaZaznaczona.setTextColor(Color.BLACK);
             buttonIncrement.setTextColor(Color.WHITE);
-            buttonIncrement.setBackgroundColor(Color.BLUE);
             switcher.setTextColor(Color.BLACK);
         }
-        });
-
-        checkbox = findViewById(R.id.check);
-        textViewOpcjaZaznaczona = findViewById(R.id.inform);
-
-        // ten kod da nam nasluchiwanie na zmiane check czy zaznaczony czy nie
-        checkbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // Jeśli zaznaczony to wyświetli się text
-                    textViewOpcjaZaznaczona.setVisibility(View.VISIBLE);
-                } else {
-                    // Jeśli odznaczony to ukryje text
-                    textViewOpcjaZaznaczona.setVisibility(View.GONE);
-                }
-            }
-        });
-
     }
-    @Override
-    protected  void onSaveInstanceState(Bundle outState){
-        super.onSaveInstanceState(outState);
-        outState.putInt(KEY_COUNT, count);
-        outState.putString(KEY_INPUT, input2);
-        outState.putBoolean(KEY_CHECK, check2);
-        outState.putBoolean(KEY_SWITCH, switch2);
-    }
-    protected void updateCountText(){
-        textViewCount.setText("Licznik: "+ count);
-    }
-
 }
